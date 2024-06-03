3139324916c8f7107df713deb9fa3ac306887f87
zmarabeas <zmarabeas@github.com>
2024-06-03 13:33:04 -0400

    add lead details page

--- src/routes/leads/+page.svelte
+++ src/routes/leads/+page.svelte
@@ -55,6 +55,28 @@
     console.log(leads);
   }
 
+  let selectedLead = null;
+
+  $: {
+    console.log(selectedLead);
+  }
+
+  function getQuestionInfo(question) {
+    let ret = '';
+    if (typeof question === 'object') {
+      Object.keys(question).forEach(key => {
+        if (question[key] !== '') {
+          ret += `${key}: ${question[key]}\n`;
+        }
+      });
+    } else {
+      ret = question;
+    }
+    if (ret === '') {
+      ret = null;
+    }
+    return ret;
+  }
 
 </script>
 
@@ -62,31 +84,78 @@
 
 <div class=main>
   <div class=container>
-    <div class=lead-info>
-      <p>Name</p>
-      <p>Phone</p>
-      <p>Email</p>
-      <p>Type</p>
-      <p>Status</p>
-    </div>
 
-    {#each leads as lead}
+    {#if leads.length === 0}
+      <p>No leads found</p>
+    {:else if selectedLead !== null}
       <div class=wrapper>
+        <button  class=back on:click={() => selectedLead = null}>Back</button>
         <div class=lead-info>
-          <p>{lead.name}</p>
-          <p>{lead.phone || '-'}</p>
-          <p>{lead.email || '-'}</p>
-          <p>{lead.type}</p>
-          <p>{lead.status}</p>
+          <h2>{selectedLead.name}</h2>
+          <h2>{selectedLead.phone || '-'}</h2>
+          <h2>{selectedLead.email || '-'}</h2>
+          <h2>Type: {selectedLead.type}</h2>
+          <h2>Status: {selectedLead.status}</h2>
         </div>
+        <h2>Question information</h2>
+        <div class=divider></div>
+        {#each Object.keys(selectedLead.questions) as question}
+          {#if typeof(selectedLead.questions[question]) === 'object'}
+            {#if getQuestionInfo(selectedLead.questions[question]) !== null}
+                <p id=question>{question}</p>
+                {#each Object.keys(selectedLead.questions[question]) as subQuestion}
+                    {#if getQuestionInfo(selectedLead.questions[question]) !== null}
+                      <div class=lead-info>
+                        <p>{subQuestion}</p>
+                        <p>{selectedLead.questions[question][subQuestion]}</p>
+                      </div>
+                    {/if}
+                {/each}
+                <div class=divider></div>
+              {/if}
+            {:else}
+              {#if selectedLead.questions[question] !== ''}
+                <div class=lead-info>
+                  <p id=question>{question}</p>
+                  <p>{selectedLead.questions[question]}</p>
+                </div>
+                <div class=divider></div>
+            {/if}
+          {/if}
+        {/each}
         <div class=actions>
-          <button on:click={() => updateLeadStatus(lead, 'contacted')}>Contacted</button>
-          <button on:click={() => updateLeadStatus(lead, 'closed')}>Closed</button>
-          <button on:click={() => deleteLead(lead)}>Delete</button> 
+          <button on:click={() => updateLeadStatus(selectedLead, 'contacted')}>Contacted</button>
+          <button on:click={() => updateLeadStatus(selectedLead, 'closed')}>Closed</button>
+          <button on:click={() => deleteLead(selectedLead)}>Delete</button> 
         </div>
       </div>
-      <div class=divider></div>
-    {/each}
+    {:else}
+      {#each leads as lead}
+        <div class=lead-info>
+          <p>Name</p>
+          <p>Phone</p>
+          <p>Email</p>
+          <p>Type</p>
+          <p>Status</p>
+        </div>
+        <div class=wrapper>
+          <div class=lead-info>
+            <p>{lead.name}</p>
+            <p>{lead.phone || '-'}</p>
+            <p>{lead.email || '-'}</p>
+            <p>{lead.type}</p>
+            <p>{lead.status}</p>
+          </div>
+          <div class=actions>
+            <button on:click={() => selectedLead = lead}>View</button>
+            <button on:click={() => updateLeadStatus(lead, 'contacted')}>Contacted</button>
+            <button on:click={() => updateLeadStatus(lead, 'closed')}>Closed</button>
+            <button on:click={() => deleteLead(lead)}>Delete</button> 
+          </div>
+        </div>
+        <div class=divider></div>
+      {/each}
+    {/if}
   </div>
 </div>
 
@@ -95,14 +164,28 @@
     padding: 1rem;
   }
 
+  .back {
+    align-self: flex-start;
+  }
+
+  h1 {
+    margin: 1rem;
+    margin-top: 1rem;
+  }
+
+  #question {
+    font-weight: bold;
+  }
+
   .actions {
     display: flex;
-    gap: 14rem;
+    gap: 1rem;
   }
 
   .divider {
-    height: 1px;
+    height: 2px;
     background-color: #333;
+    border-radius: 1px;
     width: 100%;
   }
 
